3d19f00b487f66c8462ccb7966729936b2631be5
Gerardo Regalado <gerardo.regalado24@gmail.com>
2025-03-16 13:55:20 -0600

    Fix: New folder inside part1 with the objective of create separations between exercises in each part

---
+++ part1/Exercises_1.1-1.2/src/App.jsx
@@ -0,0 +1,71 @@
+const Header = ({course}) => {
+
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  )
+}
+
+const Part = ({partNumber, exercises}) => {
+
+  return (
+    <>
+      <p>{partNumber} {exercises}</p>
+    </>
+  )
+}
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+
+  return (
+    <>  
+      <Part partNumber={part1} exercises={exercises1}/>
+      <Part partNumber={part2} exercises={exercises2}/>
+      <Part partNumber={part3} exercises={exercises3}/>
+    </>
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+
+  const total = exercises1 + exercises2 + exercises3
+
+  return (
+    <p>Number of exercises: {total}</p>
+
+  )
+}
+
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  )
+}
+
+export default App
