bfba5eb4c3a41d79dacabf8ccc41c015078fc05a
Gerardo Regalado <gerardo.regalado24@gmail.com>
2025-03-25 14:05:09 -0600

    Exercise 2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,31 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [filteredContacts, setFilteredContacts] = useState([])
 
 
   const addContact = (event) => {
     event.preventDefault()
 
     const newContact = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1  //in order to do not use index included in map method (line 65)
     }
     const alreadyExist = persons.some(contact => contact.name === newContact.name)
 
     if(!alreadyExist) {
       setPersons(persons.concat(newContact))
       setNewName('')
+      setNewNumber('')
     } else {
       alert(`${newName} already exist`)
     }
@@ -28,23 +37,64 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleAddNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+
+    if (event.target.value === '') {
+      setFilteredContacts([])
+      return
+    }
+
+    const filtered = persons.filter(person => person.name.includes(event.target.value))
+
+      console.log(filtered)
+      setFilteredContacts(filtered)
+  }
+
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <div>Filter shown with: <input value={search} onChange={handleSearch} /> </div> 
+
+      <h2>Add a new</h2>
+
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleAddContact}/>
         </div>
+
+        <div>
+          number: <input value={newNumber} onChange={handleAddNumber} />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
 
+
+
+      <h2>Numbers</h2>
       <div>
-        {persons.map((person, i) =>
-          <p key={i}>{person.name}</p>
+        {filteredContacts.length > 0 ? (
+          filteredContacts.map((filter) =>
+            <div key={filter.id}>
+              <p>{filter.name} <span>{filter.number}</span></p>
+            </div>
+          )
+        ): (
+          persons.map((person) =>
+            <div key={person.id}>
+              <p>{person.name} <span>{person.number}</span></p>
+            </div>
+            )
+    
         )}
       </div>
     </div>
